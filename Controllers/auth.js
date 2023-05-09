65b0620fa70d2be6de525cf4df3380c6e6e111b7
tatiespi <tatiana.espinosa@uao.edu.co>
2023-05-09 18:53:27 -0500

    holi

---
+++ Controllers/auth.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const Usuario = require('../models/Usuario');
+
+const crearUsuario = async(req, res = express.response) => {
+    const { name, email, password } = req.body
+    try{
+        const usuario = new Usuario(req.body);
+        await usuario.save();
+
+
+        res.status(200).json({
+                ok: true,
+                usuario,
+            })
+        }catch(error){
+            console.log(error)
+            res.status(500).json({
+                ok: false,
+                error,
+            })
+            
+        }
+
+}
+
+const loginUsuario = (req, res = express.response) => {
+    res.json({
+        ok: true
+    })
+}
+const revalidarToken = (req, res = express.response) => {
+    res.json({
+        ok: true
+    })
+}
+
+module.exports = {
+    loginUsuario,
+    crearUsuario,
+    revalidarToken
+}
